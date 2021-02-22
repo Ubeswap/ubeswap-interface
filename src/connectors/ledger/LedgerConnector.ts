36127ef69988d974659a71d6a0bf7a0b7e6653f9
Ian Macalinao <github@igm.pub>
2021-02-22 00:11:50 -0600

    Wip ledger integration

--- src/connectors/ledger/LedgerConnector.ts
+++ src/connectors/ledger/LedgerConnector.ts
@@ -1,82 +1,68 @@
-import { ConnectorUpdate } from '@web3-react/types'
+import { ContractKit, newKit } from '@celo/contractkit'
+import { AddressValidation, LedgerWallet, newLedgerWalletWithSetup } from '@celo/wallet-ledger'
+import TransportWebUSB from '@ledgerhq/hw-transport-webusb'
+import { ChainId, CHAIN_INFO } from '@ubeswap/sdk'
 import { AbstractConnector } from '@web3-react/abstract-connector'
-import Web3ProviderEngine from 'web3-provider-engine'
-import { ledgerEthereumBrowserClientFactoryAsync } from '@0x/subproviders/lib/src' // https://github.com/0xProject/0x-monorepo/issues/1400
-import { LedgerSubprovider } from './LedgerSubprovider'
-import CacheSubprovider from 'web3-provider-engine/subproviders/cache.js'
-import { RPCSubprovider } from '@0x/subproviders/lib/src/subproviders/rpc_subprovider' // https://github.com/0xProject/0x-monorepo/issues/1400
+import { ConnectorUpdate } from '@web3-react/types'
+import { NETWORK_CHAIN_ID } from 'connectors'
 
-interface LedgerConnectorArguments {
-  chainId: number
-  url: string
-  pollingInterval?: number
-  requestTimeoutMs?: number
-  accountFetchingConfigs?: any
-  baseDerivationPath?: string
+class LedgerKit {
+  private closed = false
+  private constructor(public chainId: ChainId, public kit: ContractKit, public wallet: LedgerWallet) {}
+
+  public static async init(chainId: ChainId, idxs: number[]) {
+    const transport = await TransportWebUSB.create()
+    try {
+      const wallet = await newLedgerWalletWithSetup(transport, idxs, undefined, AddressValidation.never)
+      const kit = newKit(CHAIN_INFO[chainId].fornoURL, wallet)
+      return new LedgerKit(chainId, kit, wallet)
+    } catch (e) {
+      transport.close()
+      throw e
+    }
+  }
+
+  close = () => {
+    if (this.closed) {
+      return
+    }
+    this.closed = true
+    this.wallet.transport.close()
+    this.kit.stop()
+  }
 }
 
 export class LedgerConnector extends AbstractConnector {
-  private readonly chainId: number
-  private readonly url: string
-  private readonly pollingInterval?: number
-  private readonly requestTimeoutMs?: number
-  private readonly accountFetchingConfigs?: any
-  private readonly baseDerivationPath?: string
+  private kit: LedgerKit | null = null
 
-  private provider: any
-
-  constructor({
-    chainId,
-    url,
-    pollingInterval,
-    requestTimeoutMs,
-    accountFetchingConfigs,
-    baseDerivationPath
-  }: LedgerConnectorArguments) {
-    super({ supportedChainIds: [chainId] })
-
-    this.chainId = chainId
-    this.url = url
-    this.pollingInterval = pollingInterval
-    this.requestTimeoutMs = requestTimeoutMs
-    this.accountFetchingConfigs = accountFetchingConfigs
-    this.baseDerivationPath = baseDerivationPath
+  constructor() {
+    super({ supportedChainIds: [NETWORK_CHAIN_ID] })
   }
 
   public async activate(): Promise<ConnectorUpdate> {
-    if (!this.provider) {
-      const engine = new Web3ProviderEngine({ pollingInterval: this.pollingInterval })
-      engine.addProvider(
-        new LedgerSubprovider({
-          networkId: this.chainId,
-          ledgerEthereumClientFactoryAsync: ledgerEthereumBrowserClientFactoryAsync,
-          accountFetchingConfigs: this.accountFetchingConfigs,
-          baseDerivationPath: this.baseDerivationPath
-        })
-      )
-      engine.addProvider(new CacheSubprovider())
-      engine.addProvider(new RPCSubprovider(this.url, this.requestTimeoutMs))
-      this.provider = engine
+    const idxs = [0, 1, 2, 3, 4]
+    const ledgerKit = await LedgerKit.init(NETWORK_CHAIN_ID, idxs)
+    this.kit = ledgerKit
+    return {
+      provider: ledgerKit.kit.web3.currentProvider,
+      chainId: NETWORK_CHAIN_ID,
+      account: ledgerKit.wallet.getAccounts()[0]
     }
-
-    this.provider.start()
-
-    return { provider: this.provider, chainId: this.chainId }
   }
 
-  public async getProvider(): Promise<Web3ProviderEngine> {
-    return this.provider
+  public async getProvider(): Promise<any> {
+    return this.kit?.kit.web3.currentProvider ?? null
   }
 
   public async getChainId(): Promise<number> {
-    return this.chainId
+    return NETWORK_CHAIN_ID
   }
 
-  public async getAccount(): Promise<null> {
-    return this.provider._providers[0].getAccountsAsync(1).then((accounts: string[]): string => accounts[0])
+  public async getAccount(): Promise<string | null> {
+    return this.kit?.wallet.getAccounts()?.[0] ?? null
   }
 
   public deactivate() {
-    this.provider.stop()
+    this.kit?.close()
   }
 }
