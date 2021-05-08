0dc9338729f01aa99eb4531f9bdef042c456335f
Ian Macalinao <github@igm.pub>
2021-05-08 18:10:07 -0500

    Valora connection fixs

--- src/connectors/valora/useValora.ts
+++ src/connectors/valora/useValora.ts
@@ -1,5 +1,5 @@
 import { useWeb3React } from '@web3-react/core'
-import { valora } from 'connectors'
+import { valora } from 'connectors/index'
 import { useEffect, useState } from 'react'
 import { useValoraAccount } from 'state/user/hooks'
 
@@ -7,7 +7,7 @@ import { ValoraConnector } from './ValoraConnector'
 import { valoraEmitter } from './valoraUtils'
 
 /**
- * useValora handles incoming valora requests
+ * useValora handles connecting to Valora
  */
 export const useValora = () => {
   const [isInitialLoad, setIsInitialLoad] = useState<boolean>(true)
@@ -27,7 +27,10 @@ export const useValora = () => {
     // if there is a cached valora account and this is the initial page load, load it
     if (isInitialLoad && !injectedAccount && savedValoraAccount) {
       valora.setSavedValoraAccount(savedValoraAccount)
-      activate(valora, undefined, true)
+      activate(valora, undefined, true).catch((error) => {
+        console.error('[Valora Activation error]', error)
+        alert(`Error connecting to Valora: ${error.message}`)
+      })
     }
   }, [connector, injectedAccount, savedValoraAccount, activate, isInitialLoad])
 
--- src/connectors/valora/valoraUtils.ts
+++ src/connectors/valora/valoraUtils.ts
@@ -1,46 +1,121 @@
 import { ContractKit } from '@celo/contractkit'
 import {
   AccountAuthResponseSuccess,
+  DappKitRequestTypes,
   DappKitResponse,
+  DappKitResponseStatus,
   parseDappkitResponseDeeplink,
   SignTxResponseSuccess,
   TxToSignParam,
 } from '@celo/utils'
-import {
-  requestAccountAddress,
-  requestTxSig,
-  waitForAccountAuth,
-  waitForSignedTxs,
-} from '@celo-tools/use-contractkit/lib/dappkit-wallet/dappkit'
+import { requestAccountAddress, requestTxSig } from '@celo-tools/use-contractkit/lib/dappkit-wallet/dappkit'
 import EventEmitter from 'eventemitter3'
 import { identity } from 'lodash'
-import * as querystring from 'querystring'
 
-/**
- * Parses the response from Dappkit.
- * @param url
- */
-export const parseDappkitResponse = (
+const removeQueryParams = (url: string, keys: string[]): string => {
+  const params = parseSearchParamsHashAware(url)
+  const whereQuery = url.indexOf('?')
+  const urlNoSearchParams = whereQuery !== -1 ? url.slice(0, whereQuery) : url
+  keys.forEach((key) => {
+    params.delete(key)
+  })
+  if ([...params.keys()].length > 0) {
+    return `${urlNoSearchParams}?${params}`
+  }
+  return url
+}
+
+const parseSearchParamsHashAware = (url: string): URLSearchParams => {
+  const whereQuery = url.indexOf('?')
+  if (whereQuery === -1) {
+    return new URLSearchParams()
+  }
+  const searchNonDeduped = url.slice(whereQuery + 1)
+  const allSearch = searchNonDeduped.split('?')
+  const newQs = allSearch
+    .filter(identity)
+    .reduce((acc, qs) => ({ ...acc, ...Object.fromEntries(new URLSearchParams(qs).entries()) }), {})
+  return new URLSearchParams(newQs)
+}
+
+export const parseDappkitResponseDeeplinkHashAware = (
   url: string
 ):
   | (DappKitResponse & {
       requestId: string
     })
   | null => {
-  const whereQuery = url.indexOf('?')
-  if (whereQuery === -1) {
+  const realQs = parseSearchParamsHashAware(url)
+  if (!realQs.get('type') || !realQs.get('requestId')) {
     return null
   }
-  const searchNonDeduped = url.slice(whereQuery + 1)
-  const allSearch = searchNonDeduped.split('?')
-  const newQs = allSearch.filter(identity).reduce((acc, qs) => ({ ...acc, ...querystring.parse(qs) }), {})
-  const realQs = querystring.stringify(newQs)
-  const { protocol, host } = new URL(url)
-  const result = parseDappkitResponseDeeplink(`${protocol}//${host}/?${realQs}`)
-  if (!result.requestId) {
-    return null
+  return parseDappkitResponseDeeplink(`https://fakehost/?${realQs.toString()}`)
+}
+
+const localStorageKey = 'ubeswap/dappkit'
+// hack to get around deeplinking issue where new tabs are opened
+// and the url hash state is not respected (Note this implementation
+// of dappkit doesn't use URL hashes to always force the newtab experience).
+if (typeof window !== 'undefined') {
+  const params = parseSearchParamsHashAware(window.location.href)
+  if (params.get('type') && params.get('requestId')) {
+    localStorage.setItem(localStorageKey, window.location.href)
+    window.close()
   }
-  return result
+}
+
+async function waitForResponse() {
+  // eslint-disable-next-line no-constant-condition
+  while (true) {
+    // handle redirect
+    const params = parseSearchParamsHashAware(window.location.href)
+    if (params.get('type') && params.get('requestId')) {
+      localStorage.setItem(localStorageKey, window.location.href)
+    }
+
+    const value = localStorage.getItem(localStorageKey)
+    if (value) {
+      localStorage.removeItem(localStorageKey)
+      return value
+    }
+    await new Promise((resolve) => setTimeout(resolve, 100))
+  }
+}
+
+async function waitForAccountAuth(requestId: string): Promise<AccountAuthResponseSuccess> {
+  const url = await waitForResponse()
+  const dappKitResponse = parseDappkitResponseDeeplinkHashAware(url)
+  if (!dappKitResponse) {
+    throw new Error('no dappkit response')
+  }
+  if (
+    requestId === dappKitResponse.requestId &&
+    dappKitResponse.type === DappKitRequestTypes.ACCOUNT_ADDRESS &&
+    dappKitResponse.status === DappKitResponseStatus.SUCCESS
+  ) {
+    return dappKitResponse
+  }
+
+  console.warn('Unable to parse url', url)
+  throw new Error('Unable to parse Valora response')
+}
+
+async function waitForSignedTxs(requestId: string): Promise<SignTxResponseSuccess> {
+  const url = await waitForResponse()
+  const dappKitResponse = parseDappkitResponseDeeplinkHashAware(url)
+  if (!dappKitResponse) {
+    throw new Error('no dappkit response')
+  }
+  if (
+    requestId === dappKitResponse.requestId &&
+    dappKitResponse.type === DappKitRequestTypes.SIGN_TX &&
+    dappKitResponse.status === DappKitResponseStatus.SUCCESS
+  ) {
+    return dappKitResponse
+  }
+
+  console.warn('Unable to parse url', url)
+  throw new Error('Unable to parse Valora response')
 }
 
 /**
@@ -48,10 +123,22 @@ export const parseDappkitResponse = (
  */
 export const valoraEmitter = new EventEmitter()
 
+const randomString = () => (Math.random() * 100).toString().slice(0, 6)
+
 /**
  * Requests auth from the Valora app.
  */
 export const requestValoraAuth = async (): Promise<AccountAuthResponseSuccess> => {
+  // clean URL before requesting
+  window.location.href = removeQueryParams(window.location.href, [
+    'requestId',
+    'type',
+    'status',
+    'address',
+    'phoneNumber',
+    'pepper',
+  ])
+  localStorage.removeItem(localStorageKey)
   const requestId = `login-${randomString()}`
   requestAccountAddress({
     requestId,
@@ -61,8 +148,6 @@ export const requestValoraAuth = async (): Promise<AccountAuthResponseSuccess> =
   return await waitForAccountAuth(requestId)
 }
 
-const randomString = () => (Math.random() * 100).toString().slice(0, 6)
-
 /**
  * Requests a transaction from the Valora app.
  */
@@ -70,6 +155,8 @@ export const requestValoraTransaction = async (
   kit: ContractKit,
   txs: TxToSignParam[]
 ): Promise<SignTxResponseSuccess> => {
+  window.location.href = removeQueryParams(window.location.href, ['requestId', 'type', 'status', 'rawTxs'])
+  localStorage.removeItem(localStorageKey)
   const requestId = `signTransaction-${randomString()}`
   await requestTxSig(kit, txs, {
     requestId,
