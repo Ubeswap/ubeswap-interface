a49ef742301fd5d77218668f8512b2fac75e885f
Ian Macalinao <ian@ubeswap.org>
2021-06-24 17:54:11 -0500

    upgrade deps

--- src/index.tsx
+++ src/index.tsx
@@ -66,23 +66,23 @@ if (GOOGLE_ANALYTICS_ID) {
   ReactGA.initialize('test', { testMode: true, debug: true })
 }
 
-// sentry
-const sentryCfg = {
-  environment: `${environment ?? 'unknown'}-${NETWORK_CHAIN_NAME}`,
-  release: `${process.env.REACT_APP_VERCEL_GIT_COMMIT_REF ?? 'unknown'}-${
-    process.env.REACT_APP_VERCEL_GIT_COMMIT_SHA ?? 'unknown'
-  }`,
+if (process.env.REACT_APP_SENTRY_DSN) {
+  const sentryCfg = {
+    environment: `${process.env.REACT_APP_VERCEL_ENV ?? 'unknown'}`,
+    release: `${process.env.REACT_APP_VERCEL_GIT_COMMIT_REF?.replace(/\//g, '--') ?? 'unknown'}-${
+      process.env.REACT_APP_VERCEL_GIT_COMMIT_SHA ?? 'unknown'
+    }`,
+  }
+  Sentry.init({
+    dsn: process.env.REACT_APP_SENTRY_DSN,
+    integrations: [new Integrations.BrowserTracing()],
+    tracesSampleRate: 0.2,
+    ...sentryCfg,
+  })
+  console.log(`Initializing Sentry environment at release ${sentryCfg.release} in environment ${sentryCfg.environment}`)
+} else {
+  console.warn(`REACT_APP_SENTRY_DSN not found. Sentry will not be loaded.`)
 }
-Sentry.init({
-  dsn: 'https://3a59012948e049a290f5e3ff6f6f68e2@o605929.ingest.sentry.io/5745027',
-  integrations: [new Integrations.BrowserTracing()],
-  tracesSampleRate: 0.001,
-  environment: `${environment ?? 'unknown'}-${NETWORK_CHAIN_NAME}`,
-  release: `${process.env.REACT_APP_VERCEL_GIT_COMMIT_REF ?? 'unknown'}-${
-    process.env.REACT_APP_VERCEL_GIT_COMMIT_SHA ?? 'unknown'
-  }`,
-})
-console.log(`Initializing Sentry environment at release ${sentryCfg.release} in environment ${sentryCfg.environment}`)
 
 // react GA error tracking
 window.addEventListener('error', (error) => {
