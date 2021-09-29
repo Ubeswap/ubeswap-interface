678d53cc542c6f92df94e59f223929d537482587
Brian Li <brnli7@gmail.com>
2021-09-28 18:43:27 -0700

    Update modal for unsupported network

--- src/components/ChangeNetworkModal/index.tsx
+++ src/components/ChangeNetworkModal/index.tsx
@@ -1,21 +1,23 @@
-import { useContractKit } from '@celo-tools/use-contractkit'
-import { ButtonLight } from 'components/Button'
 import React from 'react'
 import { useTranslation } from 'react-i18next'
 
 import Modal from '../Modal'
 
-export default function ChangeNetworkModal() {
-  const { connect } = useContractKit()
+const ChangeNetworkModal: React.FC = () => {
   const { t } = useTranslation()
   return (
     <Modal isOpen={true} onDismiss={() => null} maxHeight={24} minHeight={24}>
       <div style={{ width: '100%', margin: '16px' }}>
-        <div style={{ marginBottom: '28px' }}>
-          <span>Unsupported network</span>
+        <div>
+          <span>{t('unsupportedNetwork')}</span>
         </div>
-        <ButtonLight onClick={() => connect().catch(console.warn)}>{t('changeNetwork')} Celo</ButtonLight>
+        <hr style={{ marginBottom: '28px' }} />
+        <div>
+          <span>{t('unsupportedNetworkDesc')}</span>
+        </div>
       </div>
     </Modal>
   )
 }
+
+export default ChangeNetworkModal
