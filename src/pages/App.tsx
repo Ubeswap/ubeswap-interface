c1ddd91f4a2079d1d8c4b1ef4afa1b8c96cb2ffe
Brian Li <brnli7@gmail.com>
2021-10-26 15:11:37 -0700

    Lowercase bridge

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -119,7 +119,7 @@ export default function App() {
               <Route exact strict path="/farm" component={Earn} />
               <Route exact strict path="/farm/:currencyIdA/:currencyIdB/:stakingAddress" component={Manage} />
               <Route exact strict path="/dualfarm/:currencyIdA/:currencyIdB/:stakingAddress" component={Manage} />
-              <Route exact strict path="/Bridge" component={Bridge} />
+              <Route exact strict path="/bridge" component={Bridge} />
               <Route component={RedirectPathToSwapOnly} />
             </Switch>
           </ErrorBoundary>
