c3b59fed17fd7065cbe1ced849c01f0af164a200
Zakariya Ahmed <zakariya.ahmed34@gmail.com>
2022-03-01 12:15:31 -0800

    use orderBookFactory to get maker and taker asset addresses and amounts

--- src/pages/LimitOrder/useOrderBroadcasted.tsx
+++ src/pages/LimitOrder/useOrderBroadcasted.tsx
@@ -1,11 +1,11 @@
 import { useContractKit, useProvider } from '@celo-tools/use-contractkit'
 import { ChainId } from '@ubeswap/sdk'
-import { LimitOrderProtocol__factory, Multicall__factory } from 'generated'
+import { LimitOrderProtocol__factory, Multicall__factory, OrderBook__factory } from 'generated'
 import React, { useEffect } from 'react'
 import { multicallBatch } from 'utils/multicall'
 import { AbiItem } from 'web3-utils'
 
-import { LIMIT_ORDER_ADDRESS } from '../../constants'
+import { LIMIT_ORDER_ADDRESS, ORDER_BOOK_ADDRESS } from '../../constants'
 import orderBookAbi from '../../constants/abis/limit/OrderBook.json'
 
 const CREATION_BLOCK = 9840049
@@ -49,12 +49,36 @@ export const useRemaining = () => {
   const { network } = useContractKit()
   const chainId = network.chainId as unknown as ChainId
   const limitOrderAddr = LIMIT_ORDER_ADDRESS[chainId]
+  const orderBookAddr = ORDER_BOOK_ADDRESS[chainId]
 
   const call = React.useCallback(async () => {
+    console.log(`orderHashForWallet ${orderHashForWallet}`)
     const multicall = Multicall__factory.connect('0x387ce7960b5DA5381De08Ea4967b13a7c8cAB3f6', provider)
-
     const limitOrder = LimitOrderProtocol__factory.connect(limitOrderAddr, provider)
 
+    const orderBookFactory = OrderBook__factory.connect(orderBookAddr, provider)
+
+    // call orderbook factory to get maker and taker asset addresses and amounts
+    const orderBookOrders = await multicallBatch(
+      multicall,
+      orderHashForWallet.map((orHash) => {
+        return {
+          target: orderBookAddr,
+          callData: orderBookFactory.interface.encodeFunctionData('orders', [orHash]),
+        }
+      }, 1_000)
+    )
+    const decodedOrderBookOrders = orderBookOrders.map((obo) => {
+      const decodedObo = orderBookFactory.interface.decodeFunctionResult('orders', obo)[0]
+      return {
+        makerAsset: decodedObo[1],
+        takerAsset: decodedObo[2],
+        makingAmount: (decodedObo[6] / 1000000000000000000).toFixed(2).toString(),
+        takingAmount: (decodedObo[7] / 1000000000000000000).toFixed(2).toString(),
+      }
+    })
+
+    // get open or completed order status
     const orderRemaining = await multicallBatch(
       multicall,
       orderHashForWallet.map((orHash) => {
@@ -64,14 +88,17 @@ export const useRemaining = () => {
         }
       }, 1_000)
     )
-    console.log('orderRemaining')
-    console.log(orderRemaining)
-
-    const andThen = orderRemaining.map((orRemain) => {
+    const decodedOrderRemaining = orderRemaining.map((orRemain) => {
       return limitOrder.interface.decodeFunctionResult('remainingRaw', orRemain)[0].toNumber()
     })
+
+    const limitOrdersForWallet = orderHashForWallet.map((orderHash, idx) => ({
+      orderHash,
+      isOrderOpen: decodedOrderRemaining[idx] === 0,
+      ...decodedOrderBookOrders[idx],
+    }))
     console.log('orderRemainingDECODED')
-    console.log(andThen)
+    console.log(limitOrdersForWallet)
   }, [orderHashForWallet, provider, limitOrderAddr])
 
   useEffect(() => {
