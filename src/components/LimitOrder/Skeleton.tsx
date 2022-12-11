6fdad96b5109ace931175628fd069a7430b3cf54
Redha Benammou <redha.benammou@gmail.com>
2022-12-11 11:03:55 +0100

    Make the limit order panel responsive

--- src/components/LimitOrder/Skeleton.tsx
+++ src/components/LimitOrder/Skeleton.tsx
@@ -5,11 +5,21 @@ export const LimitOrderLayout = styled.div`
   flex-direction: row;
   justify-content: center;
   width: 100%;
+  padding: 0 20px;
+  margin-top: 24px;
+  gap: 60px;
+  @media (max-width: 1115px) {
+    flex-direction: column-reverse;
+    align-items: center;
+  }
 `
 
 export const LeftPanel = styled.div`
   max-width: 780px;
   width: 100%;
+  @media screen and (max-width: 1115px) {
+    max-width: 420px;
+  }
 `
 
 export const RightPanel = styled.div`
