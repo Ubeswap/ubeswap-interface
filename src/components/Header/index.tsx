a69e4e744f30b04e452fa3d2d7e09e02baadbb7f
Redha Benammou <redha.benammou@gmail.com>
2022-12-11 02:25:15 +0100

    Fixing header links white-space

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -109,6 +109,7 @@ const HeaderRow = styled(RowFixed)`
 `
 
 const HeaderLinks = styled(Row)`
+  white-space: nowrap;
   justify-content: center;
   @media (max-width: 1115px) {
     padding: 1rem 0 1rem 1rem;
